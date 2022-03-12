8acac9a962dff85969789c6bdd3cddd651f76827
Bandi Revanth <dhanush24082014@gmail.com>
2022-03-12 18:04:40 +0530

    Create text-2-speech.py

---
+++ text-2-speech.py
@@ -0,0 +1,18 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+
+rate = engine.getProperty('rate')
+volume = engine.getProperty('volume')
+voices = engine.getProperty('voices')
+
+engine.setProperty('rate', 160)
+engine.setProperty('volume', 2)
+engine.setProperty('voice', voices[1].id)
+
+# Modify the value to make it say what you want. :)
+engine.say("""
+Tests
+""")
+
+engine.runAndWait()
